444ce96bf6d6c6a188a3ab08bec38c8f883223e0
Camilaux <mzapataar@unal.edu.co>
2024-03-10 14:14:14 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -9,6 +9,7 @@ espacio entre palabra y palabra.
 
 
 """
+import re
 import pandas as pd
 
 #data = pd.read_table("clusters_report.txt")
@@ -21,51 +22,47 @@ dicc = {'cluster':[],
         'cantidad_de_palabras_clave':[],
         'porcentaje_de_palabras_clave':[],
         'principales_palabras_clave':[]}
+resultados = []
 
 def ingest_data():
     cluster = open("clusters_report.txt", mode="r")
     for line in cluster:
         line = line.rstrip()
         palabras.append(line)
-    header = palabras[:2]
-    #datos = palabras[4:]
-    #print(palabras)
-    #print("hola",palabras[4][3].isdigit())
     
     for p in range(4,len(palabras)):
         linea = palabras[p]
-        n_elementos = 3  # Ejemplo: quieres separar los primeros 3 elementos
+        n_elementos = 3  
         
         if linea.strip() != "":
-            #print("linea",linea)
             if linea.split()[0].isdigit() == True:
-                #print("claro que si")
             # Dividir el texto por espacios
                 partes = linea.split()
 
             # Separar las primeras N partes y luego unir el resto
                 parte1 = partes[:n_elementos]
-                #parte2 = " ".join(partes[2:3])
                 parte2 = " ".join(partes[n_elementos:])
-                parte2 = parte2.replace('%', '').lstrip()
+                parte2 = parte2.replace('%', ' ').strip()
 
             # Combinar la parte separada y la parte unida en una lista
                 resultado = parte1 +[parte2]
-                #print("res",resultado)
             else:
-                resultado[-1] += linea.lstrip()
-                #print("linea",linea)
+                sin_espacio = re.sub(r'\s+', ' ', linea)
+                resultado[-1] += sin_espacio.rstrip()
+        resultados.append(resultado)
 
-        if int(resultado[0]) not in dicc['cluster']:
-            dicc['cluster'].append(int(resultado[0]))
-            dicc['cantidad_de_palabras_clave'].append(int(resultado[1]))
-            numero = resultado[2].replace(',', '.')
+    for r in resultados:
+        if int(r[0]) not in dicc['cluster']:
+            dicc['cluster'].append(int(r[0]))
+            dicc['cantidad_de_palabras_clave'].append(int(r[1]))
+            numero = r[2].replace(',', '.')
             dicc['porcentaje_de_palabras_clave'].append(float(numero))
-            dicc['principales_palabras_clave'].append(resultado[3])
+            texto = r[3].replace(".", "")
+            dicc['principales_palabras_clave'].append(texto)
 
     return pd.DataFrame(dicc)
 
         
 
 print(ingest_data())
-    
+    #"maximum power point tracking, fuzzy-logic based control, photo voltaic (pv), photo-voltaic system, differential evolution algorithm, evolutionary algorithm, double-fed induction generator (dfig), ant colony optimisation, photo voltaic array, firefly algorithm, partial shade"
